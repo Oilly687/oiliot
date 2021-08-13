48e15646cfde6e246bf9f72e50348d420c4f8361
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-13 15:10:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/> 
  
 <br/> <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
- <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
+<iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
+<iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="450" height="250"> </iframe>
  
