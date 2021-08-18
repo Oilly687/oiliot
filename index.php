0f0c81a719629a26ecbb8b25e6efa3ac1780b8fd
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 14:26:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <html lang="en">
 <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/> 
-https=require('https');
-https.get('https://api.thingspeak.com/channels/1458419/fields/1/last.json?key=T1WRG0V9V42XSWCH', function(res) {
-console.log("Got response: " + res.statusCode);
-}).on('error', function(e) {
-console.log("Got error: " + e.message);
-context.done(null, 'FAILURE');
-});
-
+<head>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body onload="JavaScript:timedRefresh(15000);"><BR>
+<html lang="en">
+<h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/>
+ <h3>Humidity : 2147483647 % <br>
+    Temperature: 2147483647 C <br>
+    Created at: 18-08-2021 14:19<br>
+    </h3>
 <br/> <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
 <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
 <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
-
+<script type="text/JavaScript">
+function timedRefresh(timeoutPeriod) {
+ setTimeout("location.reload(true);",timeoutPeriod);
+}
+</script>
+</body>
  
