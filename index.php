40987fb6a409f82a37039fbccfe669ac2821abee
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 16:32:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <head>
 </head>
  <body>
-      <h1>Mustakeem Laehlong 62107677</h1>
-      <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-         <p style="font-size:50px;font:bold;"> 
+  <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
+  </iframe>
+
      <?php
-        $api_url = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
+        $api_url = 'https://api.thingspeak.com/channels/1458419/fields/1.json?results=1';
         $json_data = file_get_contents($api_url);
         $response_data = json_decode($json_data);
         $user_data = $response_data->feeds;
@@ -18,9 +18,10 @@
         ?>
      </p>
       <br>
-      <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
+      <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
+       </iframe>
       <?php
-        $api_url = 'https://api.thingspeak.com/channels/1458411/fields/2.json?results=1';
+        $api_url = 'https://api.thingspeak.com/channels/1458419/fields/2.json?results=1';
         $json_data = file_get_contents($api_url);
         $response_data = json_decode($json_data);
         $user_data = $response_data->feeds;
@@ -31,6 +32,6 @@
          }
         ?>
       <br>
-      <iframe src="https://thingspeak.com/channels/1458411/maps/channel_show" style="height:250px;width:500px;" title="LIGHT"></iframe>
+      <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
 </body>
 </html>
