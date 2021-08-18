6147f8d637851ee59b31e5775091ab408ca18f2e
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 17:08:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <div class="container p-3 my-3 bg-dark text-white">
   <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 </h1>
   </div>
-  <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
+  <align = 'center' iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
   </iframe>
      <br>
      <?php
@@ -26,7 +26,7 @@
          }
         ?>
      </p>
-      <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
+      <align = 'center' iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
        </iframe>
       <br>
       <?php
@@ -41,6 +41,6 @@
         ?>
        <br>
   <br>
-      <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
+      <align = 'center' iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
 </body>
 </html>
