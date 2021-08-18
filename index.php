82c7b204f7335749f4fa77f5954b91bcfd718b99
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 16:42:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         ?>
      </p>
       <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
-       </iframe>
+       </iframe><br>
       <?php
         $api_url = 'https://api.thingspeak.com/channels/1458419/fields/2.json?results=1';
         $json_data = file_get_contents($api_url);
