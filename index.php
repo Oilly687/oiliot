5f3ef5c7e1e579c22dc15302ad8bba9029d24e72
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 15:55:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-  
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body >
+ 
 <html lang="en">
 <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/>
 <br/> <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
@@ -23,11 +14,8 @@ foreach ($user_data as $user) {
 	<?php echo "%"; ?>
 	<iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
 <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
-<script type="text/JavaScript">
-function timedRefresh(timeoutPeriod) {
- setTimeout("location.reload(true);",timeoutPeriod);
-}</script>
-</body>
+
+
       
         
  
