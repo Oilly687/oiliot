185c4cdb2a2f0d7706b9ddafb3106e76ab0bdda5
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 16:41:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,12 @@
         $user_data = $response_data->feeds;
         $user_data = array_slice($user_data, 0);
         foreach ($user_data as $user) {
-            echo "Humidity: ".$user->field1;
-            echo " %";
+            echo "Humidity: ".$user->field1; echo " %";
          }
         ?>
      </p>
       <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
        </iframe>
-    <br>
       <?php
         $api_url = 'https://api.thingspeak.com/channels/1458419/fields/2.json?results=1';
         $json_data = file_get_contents($api_url);
@@ -28,8 +26,7 @@
         $user_data = $response_data->feeds;
         $user_data = array_slice($user_data, 0);
         foreach ($user_data as $user) {
-             echo "Temperature: ".$user->field2;
-             echo " C"; 
+        echo "Temperature: ".$user->field2; echo " C"; 
          }
         ?>
       <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
