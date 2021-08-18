820e66e7c2171b25de390f67cc34ec6717d1f8ee
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 16:20:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <head>
 </head>
 <body>
-<html lang="en">
 <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/>
 <br/> 
       <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
