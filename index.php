2dcdec6bdfcb8213d1d7e53c5591f0e29eb0ce86
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 14:17:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html lang="en">
 <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/> 
-fetch('https://api.thingspeak.com/channels/1458419/feeds.json?results=2').then(function (response) {
-	// The API call was successful!
-	console.log('success!', response);
-}).catch(function (err) {
-	// There was an error
-	console.warn('Something went wrong.', err);
+https=require('https');
+https.get('https://api.thingspeak.com/channels/1458419/fields/1/last.json?key=T1WRG0V9V42XSWCH', function(res) {
+console.log("Got response: " + res.statusCode);
+}).on('error', function(e) {
+console.log("Got error: " + e.message);
+context.done(null, 'FAILURE');
 });
 
 <br/> <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
