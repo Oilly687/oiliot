418d3d288780d4115d3afdeb8f1f889d8f378d97
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 15:33:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,32 +10,32 @@
 <body >
 <html lang="en">
 <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 <br/>
- 
-
 <br/> <iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
 <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250"> </iframe>
 <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
- 
+ <?php
+$api_url = 'https://api.thingspeak.com/channels/1458419/feeds.json?results=2';
+$json_data = file_get_contents($api_url);
+$response_data = json_decode($json_data);
+$user_data = $response_channel->channel;
+$user_data = array_slice($user_data, 0, 9);
+foreach ($user_data as $user) {
+	echo "Humidity: "channel->name;
+	echo "<br />";
+	echo "Temperature: "feeds[1]->field1;
+	echo "<br /> <br />";
+        echo "Created at: "feeds[1]->field2;
+	echo "<br /> <br />";
+	 echo "Created at: "feeds[1]->created_at;
+	echo "<br /> <br />";
+}
+
+?> 
 <script type="text/JavaScript">
 function timedRefresh(timeoutPeriod) {
  setTimeout("location.reload(true);",timeoutPeriod);
 }</script>
 </body>
-<?php
-$api_url = 'https://api.thingspeak.com/channels/1458419/feeds.json?results=2';
-$json_data = file_get_contents($api_url);
-$response_data = json_decode($json_data);
-$user_data = $response_data->channel;
-$user_data = array_slice($user_data, 0, 9);
-foreach ($user_data as $user) {
-	echo "Humidity: ".$user->field1;
-	echo "<br />";
-	echo "Temperature: ".$user->field2;
-	echo "<br /> <br />";
-        echo "Created at: ".$user->created_at;
-	echo "<br /> <br />";
-}
-
-?>       
+      
         
  
