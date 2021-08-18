6697d2762b992b9aad5fbd655fa2407b105064c3
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 17:14:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <h1 align = 'center'> Oilly Apaporn Thipsavak 62111687 </h1>
   </div>
   < iframe src="https://thingspeak.com/channels/1458419/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
-  < align = 'center'></iframe>
+  <center></iframe>
      <br>
      <?php
         $api_url = 'https://api.thingspeak.com/channels/1458419/fields/1.json?results=1';
@@ -27,7 +27,7 @@
         ?>
      </p>
       < iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
-       < align = 'center'></iframe>
+       <center></iframe>
       <br>
       <?php
         $api_url = 'https://api.thingspeak.com/channels/1458419/fields/2.json?results=1';
@@ -41,6 +41,6 @@
         ?>
        <br>
   <br>
-      <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"><align = 'center'> </iframe>
+      <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"><center> </iframe>
 </body>
 </html>
