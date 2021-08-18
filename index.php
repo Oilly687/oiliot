54251d4ea0c45c0a4616ced814280d016ef206e6
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-18 16:52:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,6 @@
       <iframe src="https://thingspeak.com/channels/1458419/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="250">
        </iframe>
       <br>
-  <br>
       <?php
         $api_url = 'https://api.thingspeak.com/channels/1458419/fields/2.json?results=1';
         $json_data = file_get_contents($api_url);
@@ -31,8 +30,8 @@
         echo "Temperature: ".$user->field2; echo " C"; 
          }
         ?>
-  <br>
        <br>
+  <br>
       <iframe src="https://thingspeak.com/channels/1458419/maps/channel_show" width="455" height="260"> </iframe>
 </body>
 </html>
